ebda49bb3f85cd08089d5e1c1fecf06e54d2ae28
Robert <robert.j.rantanen@helsinki.fi>
2019-04-10 14:44:48 +0300

    käyttäjänimet uniikkeja

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -3,15 +3,15 @@ from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, validators
   
 class LoginForm(FlaskForm):
-    username = StringField("Käyttäjänimi",[validators.Length(min=1, message="Käyttäjänimen pitää olla vähintään 1 merkkiä pitkä")])
-    password = PasswordField("Salasana",[validators.Length(min=1, message="Salasanan pitää olla vähintään 1 merkkiä pitkä")])
+    username = StringField("Käyttäjänimi",[validators.Length(min=1, max=144, message="Käyttäjänimen täytyy olla vähintään 1 merkkiä pitkä")])
+    password = PasswordField("Salasana",[validators.Length(min=1, max=144, message="Salasanan täytyy olla vähintään 1 merkkiä pitkä")])
   
     class Meta:
         csrf = False
 
 class RegisterForm(FlaskForm):
-    username = StringField("Käyttäjänimi",[validators.Length(min=1, message="Käyttäjänimen pitää olla vähintään 1 merkkiä pitkä")])
-    password = PasswordField("Salasana",[validators.Length(min=1, message="Salasanan pitää olla vähintään 1 merkkiä pitkä"),  validators.EqualTo("confirm", message='Salasanat eivät täsmää')])
+    username = StringField("Käyttäjänimi",[validators.Length(min=1, max=144, message="Käyttäjänimen täytyy olla vähintään 1 merkkiä pitkä")])
+    password = PasswordField("Salasana",[validators.Length(min=1, max=144, message="Salasanan täytyy olla vähintään 1 merkkiä pitkä"),  validators.EqualTo("confirm", message='Salasanat eivät täsmää')])
     confirm = PasswordField("Vahvista salasana")
   
     class Meta:
--- application/auth/models.py
+++ application/auth/models.py
@@ -7,7 +7,7 @@ class User(Base):
 
     __tablename__ = "account"
 
-    username = db.Column(db.String(144), nullable=False)
+    username = db.Column(db.String(144), nullable=False, unique=True)
     password = db.Column(db.String(144), nullable=False)
 
     kurssi = relationship('Varaus', backref=db.backref('varaus.kurssi'))
